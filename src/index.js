d67c7f8a933d8ddee425b2f06b456375c96c46df
Ahmed Ayman <ahmed.ayman93@gmail.com>
2018-05-18 00:40:15 +0200

    Added styles reset, base styles and css variables for fonts, colors & styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.scss';
 import configureStore from './store/store';
 import App from './components/App';
+import './theme/index.scss';
 
 const store = configureStore();
 

8f37d9720576e047776ea627e464fbe47d97160f
Ahmed Ayman <ahmed.ayman93@gmail.com>
2018-05-18 00:38:45 +0200

    Added styles reset, base styles and css variables for fonts, colors & styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
-const App = () => <div>Hello World!</div>;
+const App = () => <div style={{ padding: "16px" }}>Hello World!</div>;
 
 export default hot(module)(App);

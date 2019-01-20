873196a50af14fae6c71916bb5036c4eb7e1aaac
Ahmed Ayman <ahmed.ayman93@gmail.com>
2019-01-20 06:36:49 +0200

    changed the devtool extension  method as the current method is depricated

--- src/store/storeConfig/configureStore.dev.js
+++ src/store/storeConfig/configureStore.dev.js
@@ -2,13 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const enhancer = compose(
-  applyMiddleware(thunkMiddleware),
-  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
-    ? window.devToolsExtension()
-    : f => f
-);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
 
 export default function configureStore() {
   return createStore(rootReducer, enhancer);
 }
+/* eslint-enable */
